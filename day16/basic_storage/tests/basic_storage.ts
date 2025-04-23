87ebbb7941adbd1e09bfb697e0b9922dd7a56af2
Augustin Villetard <augustin.dev@tutamail.com>
2025-04-23 12:43:58 +0900

    accounts

---
+++ day16/basic_storage/tests/basic_storage.ts
@@ -0,0 +1,21 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { BasicStorage } from "../target/types/basic_storage";
+
+describe("basic_storage", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.basicStorage as Program<BasicStorage>;
+
+  it("Is initialized!", async () => {
+    const seeds = [];
+    const [myStorage, _bump] =
+    anchor.web3.PublicKey.findProgramAddressSync(seeds, program.programId);
+
+    console.log("the storage account address is: ",myStorage.toBase58());
+
+    // Add your test here.
+    await program.methods.initialize().rpc();
+  });
+});
